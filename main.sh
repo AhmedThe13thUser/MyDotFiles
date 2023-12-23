f5aac3626642cd2667a781edbb151e16b10aff3f
AhmedThe13thUser <118510784+AhmedThe13thUser@users.noreply.github.com>
2023-12-23 15:54:23 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,8 +4,11 @@ if [ "$UID" -eq 0 ]; then
   sudo rm -rf ~/.config
   sudo rm -rf ~/.zshrc
   sudo rm -rf ~/.gitconfig
-
-  sudo cp ./.config ./.zshrc ./.gitconfig ../
+  
+  sudo cp -r ./.config/ ~/.config/
+  sudo cp -r ./.zshrc ~/.zshrc
+  sudo cp -r ./.gitconfig ~/.gitconfig
+  
 else
   echo "You are just an ordinary user. please run as root (use sudo)" >&2
 fi
